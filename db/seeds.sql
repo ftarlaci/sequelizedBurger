ba1135a588a4453c0a58da89cabb276ee9978d21
ftarlaci <ftarlaci@utexas.edu>
2016-07-31 18:51:24 -0500

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Healthy Cheese Burger', FALSE, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Greek Burger', FALSE, now());
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Happy Burger', FALSE, now());
+	
